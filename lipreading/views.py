8430274e0dc5a52f1b11995a1e4dcb2d16cbb817
Meet2674 <meetshah633338@gmail.com>
2021-10-21 22:57:02 +0530

    correction module updated

--- lipreading/views.py
+++ lipreading/views.py
@@ -2,9 +2,10 @@ import mouth_detection
 import correction_module_code
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
-  
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 def home_view(request):
     return render(request, "home.html")
@@ -16,10 +17,10 @@ def mouth_detect_view(request):
 def correction_module_view(request):
     return render(request, "correction_module.html")
 
+@csrf_exempt
 def correction_module_view_use(request):
     
-    final_value = correction_module_code.run_this('there are too apples')
+    text = request.body.decode('utf8').replace("'", '"').replace("+", " ").replace("data=", "")
+    final_value = correction_module_code.run_this(text)
     print(final_value)
-    context = {}
-    context["final_value"] =final_value
-    return render(request, "correction_module.html", context)
+    return StreamingHttpResponse(final_value)
