ea2af1b47cb1c06b383437b4fe332799c46b1325
Nishant Dave <davenishant2k@gmail.com>
2022-03-31 19:11:59 +0530

    added login page

--- myfypproject/urls.py
+++ myfypproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from lipreading.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 
@@ -30,3 +32,5 @@ urlpatterns = [
     path('mix_module', mix_module_view, name = 'mix'),
     path('admin/', admin.site.urls),
 ]
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
